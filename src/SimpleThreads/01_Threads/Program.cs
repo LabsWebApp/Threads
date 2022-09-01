54e80a2eb794fa3a74e9f1c12a200dad53fe7d0f
Vladimir <vovikdoc@gmail.com>
2022-09-01 07:52:06 +0700

    #First project

---
+++ src/SimpleThreads/01_Threads/Program.cs
@@ -0,0 +1,22 @@
+﻿// Потоки.
+
+// Метод, который планируется выполнять в отдельном потоке.
+static void WriteSecond()
+{
+    while (true)
+    {
+        Console.WriteLine(new string(' ', 10) + "Secondary");
+    }
+}
+
+ThreadStart writeSecond = WriteSecond;
+Thread thread = new Thread(writeSecond);
+thread.Start();
+
+while (true)
+{
+    Console.WriteLine("Primary");
+}
+
+// Delay.
+Console.ReadKey();
